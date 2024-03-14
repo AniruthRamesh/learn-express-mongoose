295e54e0a4baea8ae8e12a3a9a4eb98a41bb9c8d
Aniruth Ramesh <anikrishna3@gmail.com>
2024-03-14 16:22:22 -0400

    Completed class activities

--- pages/books_status.js
+++ pages/books_status.js
@@ -1,5 +1,15 @@
 let BookInstance = require('../models/bookinstance');
+let book = require('../models/book')
 
-exports.show_all_books_status = function(res) {
-  return res.send([]);
+exports.show_all_books_status = async function(res) {
+  const allBooks = await BookInstance.find({}).populate('book').exec()
+  const available = allBooks.filter(bookins => {
+    return bookins.status === 'Available'
+  });
+  const availableBooks = available.map(avail => {
+    
+    return `${avail.book.title} - Available`;
+  })
+
+  return res.send(availableBooks);
 }
